10de10b767a7ba986efc9f2c76baa17b104f8df9
SimoSabev <sabevsimeon08@gmail.com>
2024-10-21 01:47:39 +0300

    style(navbar): styled the whole working navbar template

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,106 @@
-import Image from "next/image";
+"use client"
+import React, { useState } from "react";
+import { HeroParallax } from "@/components/ui/hero-parallax";
+import train1 from "@/assets/train1.jpg";
+import train2 from "@/assets/train2.jpg";
+import train3 from "@/assets/train3.jpg";
+import train4 from "@/assets/train4.jpg";
+import train5 from "@/assets/train5.jpg";
+import train6 from "@/assets/train6.jpg";
+import train7 from "@/assets/train7.jpg";
+import train8 from "@/assets/train8.jpg";
+import train9 from "@/assets/train9.jpg";
+import train10 from "@/assets/train10.jpg";
+import train11 from "@/assets/train11.jpg";
+import train12 from "@/assets/train12.jpg";
+import train13 from "@/assets/train13.jpg";
+import train14 from "@/assets/train14.jpg";
+import train15 from "@/assets/train15.jpg";
+import { NavbarDemo } from "@/components/ui/Nav";
+
+const products = [
+  {
+    title: "Product 1",
+    link: "/product-1",
+    thumbnail: train1.src,
+  },
+  {
+    title: "Product 2",
+    link: "/product-2",
+    thumbnail: train2.src,
+  },
+  {
+    title: "Product 3",
+    link: "/product-3",
+    thumbnail: train3.src,
+  },
+  {
+    title: "Product 4",
+    link: "/product-4",
+    thumbnail: train4.src,
+  },
+  {
+    title: "Product 5",
+    link: "/product-5",
+    thumbnail: train5.src,
+  },
+  {
+    title: "Product 1",
+    link: "/product-1",
+    thumbnail: train6.src,
+  },
+  {
+    title: "Product 2",
+    link: "/product-2",
+    thumbnail: train7.src,
+  },
+  {
+    title: "Product 3",
+    link: "/product-3",
+    thumbnail: train8.src,
+  },
+  {
+    title: "Product 4",
+    link: "/product-4",
+    thumbnail: train9.src,
+  },
+  {
+    title: "Product 5",
+    link: "/product-5",
+    thumbnail: train10.src,
+  },
+  {
+    title: "Product 1",
+    link: "/product-1",
+    thumbnail: train11.src,
+  },
+  {
+    title: "Product 2",
+    link: "/product-2",
+    thumbnail: train12.src,
+  },
+  {
+    title: "Product 3",
+    link: "/product-3",
+    thumbnail: train13.src,
+  },
+  {
+    title: "Product 4",
+    link: "/product-4",
+    thumbnail: train14.src,
+  },
+  {
+    title: "Product 5",
+    link: "/product-5",
+    thumbnail: train15.src,
+  },
+];
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+    <main>
+        <NavbarDemo />
+      <HeroParallax products={products} />
+    </main>
   );
 }
