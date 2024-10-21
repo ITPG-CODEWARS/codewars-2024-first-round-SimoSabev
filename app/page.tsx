6f13c1d49c28fbc02f79e9b8118bcd43175c4b1a
SimoSabev <sabevsimeon08@gmail.com>
2024-10-21 23:46:12 +0300

    style(site)

--- app/page.tsx
+++ app/page.tsx
@@ -103,7 +103,7 @@ export default function Home() {
         <main>
             <NavbarDemo/>
             <HeroParallax products={products}/>
-                <GlobeDemo/>
+            <GlobeDemo/>
             <FeaturesSectionDemo/>
         </main>
     );
--- components/ui/TrainCard.tsx
+++ components/ui/TrainCard.tsx
@@ -42,27 +42,15 @@ const cardData = [
     imageUrl: "https://images.unsplash.com/photo-1441974231531-c6227db76b6e?q=80&w=2560&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     link: "https://twitter.com/mannupaaji",
   },
-  {
-    title: "Make things float in air",
-    description: "Hover over this card to unleash the power of CSS perspective",
-    imageUrl: "https://images.unsplash.com/photo-1441974231531-c6227db76b6e?q=80&w=2560&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    link: "https://twitter.com/mannupaaji",
-  },
-  {
-    title: "Make things float in air",
-    description: "Hover over this card to unleash the power of CSS perspective",
-    imageUrl: "https://images.unsplash.com/photo-1441974231531-c6227db76b6e?q=80&w=2560&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    link: "https://twitter.com/mannupaaji",
-  },
   // Add more card data objects here
 ];
 
 export function ThreeDCardDemo() {
   return (
-    <div className="card-grid flex flex-wrap justify-center items-center gap-4">
+    <div className="card-grid grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 p-8">
       {cardData.map((card, index) => (
         <CardContainer key={index} className="inter-var">
-          <CardBody className="bg-gray-50 relative group/card dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl p-6 border">
+          <CardBody className="bg-gray-50 relative group/card dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-full h-full rounded-xl p-6 pb-16 border">
             <CardItem
               translateZ="50"
               className="text-xl font-bold text-neutral-600 dark:text-white"
@@ -85,24 +73,6 @@ export function ThreeDCardDemo() {
                 alt="thumbnail"
               />
             </CardItem>
-            <div className="flex justify-between items-center mt-20">
-              <CardItem
-                translateZ={20}
-                as={Link}
-                href={card.link}
-                target="__blank"
-                className="px-4 py-2 rounded-xl text-xs font-normal dark:text-white"
-              >
-                Try now →
-              </CardItem>
-              <CardItem
-                translateZ={20}
-                as="button"
-                className="px-4 py-2 rounded-xl bg-black dark:bg-white dark:text-black text-white text-xs font-bold"
-              >
-                Sign up
-              </CardItem>
-            </div>
           </CardBody>
         </CardContainer>
       ))}
