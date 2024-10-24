32e168c7c309e9b8315c6d523db3007ce0e1125c
SimoSabev <sabevsimeon08@gmail.com>
2024-10-24 10:53:29 +0300

    header position

--- train/components/ui/hero-parallax.tsx
+++ train/components/ui/hero-parallax.tsx
@@ -104,7 +104,7 @@ export const HeroParallax = ({
 
 export const Header = () => {
   return (
-    <div className="max-w-7xl relative mx-auto py-20 md:py-40 px-4 w-full  left-0 top-0">
+    <div className="max-w-7xl relative mx-auto py-20 md:py-40 px-4 w-full  left-10 top-0">
       <h1 className="text-2xl md:text-8xl font-bold dark:text-white">
         Welcome to <br /> Trainco
       </h1>
